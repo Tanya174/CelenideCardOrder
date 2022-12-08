11504b6aeea622568c23c16312ad89422c0b8dfb
Tatyana Petrova <tanyshka-petrova@mail.ru>
2022-12-08 17:43:05 +0300

    New file

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,22 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class SeleniumTest {
+    @Test
+    void shouldOrderCard() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Иванов Иван");
+        form.$("[data-test-id=phone] input").setValue("+79000000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(byText("Продолжить")).click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+    
+}
+
