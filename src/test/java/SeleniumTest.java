0dca16f17e3bd825d6cee35e2d8f5ea83b2a6aee
Tatyana Petrova <tanyshka-petrova@mail.ru>
2022-12-08 18:31:54 +0300

    Changed file

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -17,6 +17,29 @@ class SeleniumTest {
         form.$(byText("Продолжить")).click();
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
-    
+
+    @Test
+    void shouldOrderCarSecond() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Иванов-Петров Дима");
+        form.$("[data-test-id=phone] input").setValue("+79000000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(byText("Продолжить")).click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldOrderCardThird() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Иванов Иван Младший");
+        form.$("[data-test-id=phone] input").setValue("+79000000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(byText("Продолжить")).click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+
 }
 
